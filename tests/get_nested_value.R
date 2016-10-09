33d9bd1212383c8c691ce5beefdbe6e5ba31fd9a
ronkeizer <ronkeizer@gmail.com>
2016-10-09 00:23:16 -0700

    small fix

--- tests/get_nested_value.R
+++ tests/get_nested_value.R
@@ -3,5 +3,5 @@ library(json2test)
 a <- list(b = 3, c = list(d = "test"))
 b <- data.frame(c1 = c(1,2,3), c2 = c(4,5,6))
 
-testit::assert("from list", json2test:::get_nested_value(a, "c.d") == "test")
-testit::assert("from data.frame", json2test:::get_nested_value(b, "c1.2") == 2)
+json2test::assert("from list", json2test:::get_nested_value(a, "c.d") == "test")
+json2test::assert("from data.frame", json2test:::get_nested_value(b, "c1.2") == 2)
